6df6ab27a6c5b354de6688fbfc9035627f6f7fc1
Oleg Akinfiev <akinfiev91@gmail.com>
2023-07-10 22:54:12 +0300

    commit

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+	 <div className='hero'>
+		<div className='flex-1 pt-36 padding-x'>
+			<h1 className="hero__title">
+				Найдите, забронируйте или арендуйте автомобиль - быстро и легко!
+			</h1>
+			<p className='hero__subtitle'>
+				Упростите свой процесс аренды автомобиля с помощью нашего простого бронирования процесс.
+			</p>
+		</div>
+	 </div>
+  )
+}
+
+export default Hero
