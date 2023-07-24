184444afec1d197e8c45581af40746b08f7ac794
Oleg Akinfiev <akinfiev91@gmail.com>
2023-07-24 22:30:52 +0300

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-	 <div>
-		
-	 </div>
+	 <footer className='flex flex-col text-black-100 mt-5 border-t border-green-100'>
+		<div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 py-10'>
+			<div className='flex flex-col justify-start items-start gap-6'>
+				<Image src="/logo.svg" alt="logo" width={118} height={18} className='object-contain' />
+				<p className='text-base text-gray-700'>Carhub 2023 <br />
+				Все права сохранены &copy;</p>
+			</div>
+
+			<div className='footer__links'>
+
+			</div>
+		</div>
+	 </footer>
   )
 }
 
